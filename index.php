f6e02ecce88de0ae0d15fdd6562b279f27c7b13a
thanush96 <danushthanush@gmail.com>
2020-10-25 14:02:04 +0530

    Step 2 : Add Bootstrap for POP UP

--- index.php
+++ index.php
@@ -9,7 +9,27 @@
 </head>
 
 <body>
-
+    <!-- Modal -->
+    <div class="modal fade" id="studentaddmodel" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="studentaddmodellabel">Add Student Data</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    
+                    
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="container">
         <div class="jumbotron">
             <div class="card">
